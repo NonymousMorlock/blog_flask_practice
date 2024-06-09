f046a2c1e4815dbf677f5fcab43f9d17294fedb6
NonymousMorlock <agamspaul@gmail.com>
2024-06-09 23:50:42 +0000

    Remove the default configurations

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', '8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///d_blog.db')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
