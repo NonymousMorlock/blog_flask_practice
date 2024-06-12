cf85c0da18692c28d63fd5289217bc6592b09a08
NonymousMorlock <agamspaul@gmail.com>
2024-06-12 21:53:26 +0000

    Create __init__.py for app module

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+# from flask_gravatar import Gravatar
+from flask_login import LoginManager
+
+from config import Config
+from database import db
+from app.utils import gravatar_url
+
+
+def create_app() -> Flask:
+    flask_app = Flask(__name__)
+    flask_app.config.from_object(Config)
+    ckeditor = CKEditor()
+    bootstrap5 = Bootstrap5()
+    login_manager = LoginManager()
+
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(flask_app)
+    ckeditor.init_app(flask_app)
+    bootstrap5.init_app(flask_app)
+    db.init_app(flask_app)
+
+    flask_app.jinja_env.filters['gravatar'] = gravatar_url
+
+    with flask_app.app_context():
+        __import__('app.models')
+        db.create_all()
+
+    @login_manager.user_loader
+    def load_user(user_id: str):
+        from app.models import User
+        return User.query.filter_by(alternative_id=user_id).first()
+
+    from app.controllers import register_blueprints
+    register_blueprints(flask_app)
+
+    return flask_app
