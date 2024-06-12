8d660fe9d2d44cf386969179b69a29d35e6a79f5
NonymousMorlock <agamspaul@gmail.com>
2024-06-12 20:50:42 +0000

    Add default Database url

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///d_blog.db')
